63166280d01f6988f5366abd8943c0bd601d9e4f
Dzmitry Zakhar <31858665+SpiritForest@users.noreply.github.com>
2018-03-04 20:34:19 +0300

    1
    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  var mass = [], massCounter = [];
+  var counter = 1; maxSequence = 1;
+  mass = array.sort(function(a, b){return a - b});
+
+  if (mass.length == 0) return maxSequence = 0;
+  if (mass.length == 1) return maxSequence = 1;
+
+  for (var i = 0; i < mass.length; i++){
+  	if (i == mass.length - 1) break;
+  	if (mass[i] == mass[i+1]) continue;
+  	if (mass[i] + 1 == mass[i+1]) {counter++;}
+  	if (mass[i] + 1 != mass[i+1]) {
+  		if (counter > maxSequence) maxSequence = counter;
+  		counter = 1;
+  	}
+  }
+  return maxSequence;
 }
